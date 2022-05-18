8d08c1fa41136ca6f44febd8a8604306af8ed6ed
uczburak <uczbrk@gmail.com>
2022-05-18 06:13:20 +0300

    Add project files.

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class CarImage
+    {
+        public int ImageId { get; set; }
+        public int CarId { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
